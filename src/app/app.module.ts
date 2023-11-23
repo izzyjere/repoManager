a0f62e387a07180cd46ce77cc8ce68d3f72ae989
izzyjere <wisdomjere5@gmail.com>
2023-11-23 17:12:08 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import {SidebarComponent} from './layout/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
