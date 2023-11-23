b946cf727ceda632bc3547ca8670f9e18bda992c
izzyjere <wisdomjere5@gmail.com>
2023-11-23 17:39:43 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'signin', component: SigninComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
